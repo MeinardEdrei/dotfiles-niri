165809856e23fdd8173e0e917ba7e5626779c224
MeinardEdrei <meinardsantoss@gmail.com>
2025-10-15 18:38:22 +0800

    Commented out hyprshot dir

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 
 starship init fish | source
-set -Ux HYPRSHOT_DIR "$HOME/Pictures/Screenshots"
+# set -Ux HYPRSHOT_DIR "$HOME/Pictures/Screenshots"
 
 # cmdline-tools path
 set -g PATH $HOME/Android/Sdk/cmdline-tools/latest/bin $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/emulator $PATH
