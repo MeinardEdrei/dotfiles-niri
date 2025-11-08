389301d2eac3587257aa3dd913b27e1e7ba43ba7
MeinardEdrei <meinardsantoss@gmail.com>
2025-11-08 19:34:28 +0800

    new neotree config

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
 	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons", -- required for file icons
 		"MunifTanjim/nui.nvim",
 	},
 	cmd = "Neotree",
@@ -14,6 +14,26 @@ return {
 		{ "<C-n>", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
 	},
 	opts = {
+		default_component_configs = {
+			git_status = {
+				symbols = {
+					added = "", -- Check mark (U+F00C)
+					deleted = "", -- Minus box (U+F044)
+					modified = "", -- Pencil (U+F54B)
+					renamed = "➜", -- Heavy right-pointing arrow (U+279C) -- NEW
+					untracked = "", -- Question mark (U+F128)
+					ignored = "", -- Eye-slash (U+F474)
+					unstaged = "●", -- Black circle (U+25CF) -- NEW
+					staged = "", -- Checkered flag/Another check mark (U+F0C8)
+					conflict = "", -- Zap/lightning (U+E727)
+				},
+			},
+			icon = {
+				folder_closed = "", -- Folder icon (U+E6C0)
+				folder_open = "", -- Open folder icon (U+E6C6)
+				folder_empty = "  ", -- Empty folder icon (U+FACC)
+			},
+		},
 		filesystem = {
 			window = {
 				mappings = {
