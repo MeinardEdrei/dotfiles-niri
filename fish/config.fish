faaaeb7bb74f05eca1d9eb339ba3480930e65e90
MeinardEdrei <meinardsantoss@gmail.com>
2025-11-15 00:52:35 +0800

    updated java path

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,13 @@ end
 starship init fish | source
 # set -Ux HYPRSHOT_DIR "$HOME/Pictures/Screenshots"
 
+# android sdk
+set -e ANDROID_SDK_ROOT
+set -x ANDROID_HOME /home/batman/Android/Sdk
+set -x PATH $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $PATH
+
 # cmdline-tools path
-set -g PATH $HOME/Android/Sdk/cmdline-tools/latest/bin $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/emulator $PATH
+# set -g PATH $HOME/Android/Sdk/cmdline-tools/latest/bin $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/emulator $PATH
 
 # flutter
 set -U fish_user_paths /opt/flutter/bin $fish_user_paths
